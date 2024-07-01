da7e92d2e9127feeac5a4b53bb5542cc0414af5f
Nguyễn Anh Kiệt <nakiet22@clc.fitus.edu.vn>
2024-07-01 17:50:07 +0700

    Update permissions

--- src/backend/base/views.py
+++ src/backend/base/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 from base import permissions
@@ -14,12 +14,12 @@ from base import permissions
 
 #Anime list and details view
 class AnimeList(generics.ListCreateAPIView):
-    permission_classes = (permissions.MyCustomPermission, )
+    permission_classes = (IsAuthenticatedOrReadOnly, )
     queryset = Anime.objects.all()
     serializer_class = AnimeSerializers
     
 class AnimeDetails(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (permissions.MyCustomPermission, )
+    permission_classes = (IsAuthenticatedOrReadOnly, )
     queryset = Anime.objects.all()
     serializer_class = AnimeSerializers
 

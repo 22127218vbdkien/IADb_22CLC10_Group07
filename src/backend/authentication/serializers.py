538f603c4e64ad59ff097e452e893b07a6624ab4
Nguyễn Anh Kiệt <nakiet22@clc.fitus.edu.vn>
2024-08-02 08:59:24 +0700

    Update serializers.py
    Update user create and login form

--- src/backend/authentication/serializers.py
+++ src/backend/authentication/serializers.py
@@ -1,8 +1,23 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
+from django.contrib.auth import get_user_model
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(min_length=5,validators=[MinLengthValidator(5)],max_length=30,write_only=True,required=True)
+    username = serializers.CharField(min_length=5,validators=[MinLengthValidator(5)],max_length=30,required=True)
+    email = serializers.EmailField(required=True)
+    
+    def validate_email(self, value):
+        if get_user_model().objects.filter(email=value).exists():
+            raise serializers.ValidationError("Email already exists")
+        return value
+
+    def validate_username(self, value): 
+        if get_user_model().objects.filter(username=value).exists():
+            raise serializers.ValidationError("Username already exists") 
+        return value
+    
     class Meta:
         model = User
-        fields = ['id', 'password', 'username', 'email']
+        fields = ['id', 'username', 'password', 'email']

7c69c2344bfa1be4fd7e8ed601e23d930f6bbc17
NguyenTheTrung1212 <nttrung22@clc.fitus.edu.vn>
2024-08-29 17:51:17 +0700

    missing div tag

--- src/frontend_vue/src/view/HomeView.vue
+++ src/frontend_vue/src/view/HomeView.vue
@@ -4,75 +4,76 @@
 
 <template>
     Home View    
-    <div class="section">
-        <div class="watching">
-            <h1 class ="split">Your Watching</h1>
-            <h1 href="#">view all </h1>
-            
-        </div>
+        <div class="section">
+            <div class="watching">
+                <h1 class ="split">Your Watching</h1>
+                <h1 href="#">view all </h1>
+                
+            </div>
 
-        <br>
-        
-        <div class="anime-list">
-            <div class ="box">
-                <div class="pixel"><img src="ex.png"></div>
-                <div class="pixel"><img src="ex.png"></div>
-                <div class="pixel"><img src="ex.png"></div>
-                <div class="pixel"><img src="ex.png"></div>
-                <div class="pixel"><img src="ex.png"></div>
+            <br>
+            
+            <div class="anime-list">
+                <div class ="box">
+                    <div class="pixel"><img src="ex.png"></div>
+                    <div class="pixel"><img src="ex.png"></div>
+                    <div class="pixel"><img src="ex.png"></div>
+                    <div class="pixel"><img src="ex.png"></div>
+                    <div class="pixel"><img src="ex.png"></div>
+                </div>
+                
             </div>
             
-        </div>
-        
-        <div class="trending-anime">
-            <h1 class ="split">Trending Anime</h1>
-            <h1 href="#">view all </h1>
-        </div> 
+            <div class="trending-anime">
+                <h1 class ="split">Trending Anime</h1>
+                <h1 href="#">view all </h1>
+            </div> 
 
-        <br>
-        
-        <div class="anime-list">
-            <div class ="box">
-                <div class="pixel"><img src="ex.png"></div>
-                <div class="pixel"><img src="ex.png"></div>
-                <div class="pixel"><img src="ex.png"></div>
-                <div class="pixel"><img src="ex.png"></div>
-                <div class="pixel"><img src="ex.png"></div>
+            <br>
+            
+            <div class="anime-list">
+                <div class ="box">
+                    <div class="pixel"><img src="ex.png"></div>
+                    <div class="pixel"><img src="ex.png"></div>
+                    <div class="pixel"><img src="ex.png"></div>
+                    <div class="pixel"><img src="ex.png"></div>
+                    <div class="pixel"><img src="ex.png"></div>
+                </div>
+                
+            </div>
+
+            <div class="new-anime">
+                <h1 class ="split">New Anime</h1>
+                <h1 href="#">view all </h1>
+            </div>
+
+            <br>
+            
+            <div class="anime-list">
+                <div class ="box">
+                    <div class="pixel"><img src="ex.png"></div>
+                    <div class="pixel"><img src="ex.png"></div>
+                    <div class="pixel"><img src="ex.png"></div>
+                    <div class="pixel"><img src="ex.png"></div>
+                    <div class="pixel"><img src="ex.png"></div>
+                </div>
+            </div>
+
+            <div class="popular-anime">
+                <h1 class ="split">Popular Anime</h1>
+                <h1 href="#">view all</h1>
             </div>
             
-        </div>
-
-        <div class="new-anime">
-            <h1 class ="split">New Anime</h1>
-            <h1 href="#">view all </h1>
-        </div>
-
-        <br>
-        
-        <div class="anime-list">
-            <div class ="box">
-                <div class="pixel"><img src="ex.png"></div>
-                <div class="pixel"><img src="ex.png"></div>
-                <div class="pixel"><img src="ex.png"></div>
-                <div class="pixel"><img src="ex.png"></div>
-                <div class="pixel"><img src="ex.png"></div>
-            </div>
-        </div>
-
-        <div class="popular-anime">
-            <h1 class ="split">Popular Anime</h1>
-            <h1 href="#">view all</h1>
-        </div>
-        
-        <br>
-        
-        <div class="anime-list">
-            <div class ="box">
-                <div class="pixel"><img src="ex.png"></div>
-                <div class="pixel"><img src="ex.png"></div>
-                <div class="pixel"><img src="ex.png"></div>
-                <div class="pixel"><img src="ex.png"></div>
-                <div class="pixel"><img src="ex.png"></div>
+            <br>
+            
+            <div class="anime-list">
+                <div class ="box">
+                    <div class="pixel"><img src="ex.png"></div>
+                    <div class="pixel"><img src="ex.png"></div>
+                    <div class="pixel"><img src="ex.png"></div>
+                    <div class="pixel"><img src="ex.png"></div>
+                    <div class="pixel"><img src="ex.png"></div>
+                </div>
             </div>
         </div>
 </template>

cb29bcd3ad5922ab4501908cab3834fa03a82b92
22127252VoVietLong <vvlong22@clc.fitus.edu.vn>
2024-08-29 08:47:33 +0700

    Import Anime Search to Anime browse

--- src/frontend_vue/src/view/anime/AnimeBrowseView.vue
+++ src/frontend_vue/src/view/anime/AnimeBrowseView.vue
@@ -1,30 +1,32 @@
 <script setup>
-    import AnimeCards from '@/components/anime/AnimeCards.vue';
-    import PaginationBar from '@/components/PaginationBar.vue';
-    import { useRoute, useRouter , onBeforeRouteUpdate} from 'vue-router';
-    import { reactive, watch } from 'vue';
-    import AnimeSearchAndFilter from '@/components/search_filter/AnimeSearchAndFilter.vue';
-    const _router = useRouter()
-    const _route = useRoute()
+import { RouterLink, useRoute, useRouter } from 'vue-router';
+import { reactive } from 'vue';
+import router from '@/router';
+import axios from 'axios';
+import AnimeSearchAndFilter from '@/components/search_filter/AnimeSearchAndFilter.vue';
 
-    const state = reactive({
-        indexPage: 1, 
-        limit: 5
-    })
-    const changePage = (event) =>{
-        console.log(event)
-        _router.push({ path: _route.fullPath, query:{page:event} })
+const animes = reactive({
+    state: {}
+})
+
+const searchAnime = async () => {
+    try{
+        const response = await  axios.get('/api/animes/',{
+            params:{ 
+                anime_format: "",
+                status:"",
+                source:""
+            }
+        })
+        animes.state = response.data
+        console.log(animes.state)
+    } catch (error) {
+        console.log(error)
     }
+} 
 
-    watch(() => _route.query.page ? Number(_route.query.page) :state.indexPage,
-        (newPage) => {
-        if (state.indexPage != Number(newPage) && newPage)
-            state.indexPage = Number(newPage)
-    })
-    
 </script>
 
 <template>
-    <AnimeCards :fecthpage="state.indexPage" :key="state.indexPage"></AnimeCards>
-    <PaginationBar v-bind:_start="state.indexPage" v-bind:_limit="state.limit" :key="state.indexPage" v-on:changePage="changePage"></PaginationBar>
+    <AnimeSearchAndFilter></AnimeSearchAndFilter>
 </template>

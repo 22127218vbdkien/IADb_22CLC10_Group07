2b976a97c2c56ba2da044a7b68931d7707e29a2e
Duc Kien Van Ba <vbdkien22@clc.fitus.edu.vn>
2024-08-30 00:12:48 +0700

    added nginx.conf for frontend proxy in deployment

--- src/frontend_vue/vite.config.js
+++ src/frontend_vue/vite.config.js
@@ -4,8 +4,8 @@ import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
-const urls = ['http://127.0.0.1:8000', 'http://iadb-22clc10-group07.azurewebsites.net']
-const apiUrl = urls[1]
+const isDevelopment = process.env.NODE_ENV === 'development'
+const apiUrl = isDevelopment ? 'http://127.0.0.1:8000' : 'http://iadb-22clc10-group07.azurewebsites.net'
 
 // https://vitejs.dev/config/
 export default defineConfig({

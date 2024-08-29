fb1cd7b8b4f9441af164f0b98dc1ca2bbb2d3244
Duc Kien Van Ba <vbdkien22@clc.fitus.edu.vn>
2024-08-29 11:12:54 +0700

    fix forum bug

--- src/backend/forum/permissions.py
+++ src/backend/forum/permissions.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 
 class IsOwnerPermission(BasePermission):
     def has_object_permission(self, request, view, obj):
+        if view.action in ['list', 'retrieve']:
+            return True
         try:
             return obj.user_id == request.user
         except:

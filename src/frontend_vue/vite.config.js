70a742e46b2c1931eae356d8cff0a201a9908ea6
Duc Kien Van Ba <vbdkien22@clc.fitus.edu.vn>
2024-08-29 08:17:26 +0700

    change to deploy

--- src/frontend_vue/vite.config.js
+++ src/frontend_vue/vite.config.js
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 const isDevelopment = process.env.NODE_ENV === 'development'
 const apiUrl = isDevelopment ? 'http://127.0.0.1:8000' : 'http://iadb-22clc10-group07.azurewebsites.net'
+const baseUrl = 'https://orange-moss-01fd42600.5.azurestaticapps.net'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +14,7 @@ export default defineConfig({
     vue(),
     vueJsx(),
   ],
+  base: baseUrl,
   server:{
     port: 3030,
     proxy:{
